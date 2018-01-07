0a7a4866ef3e454299b72f4eabf85aa8b58e7703
WoSew <wojseweryn@gmail.com>
2018-01-07 13:10:53 +0100

    Saper in Console Application

--- Toci.Training/Toci.Training/Program.cs
+++ Toci.Training/Toci.Training/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Toci.Training
+﻿using System;
+
+namespace Toci.Training
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Saper example = new Saper();
+            example.SaperGame();
 
+            Console.ReadLine();
         }
     }
 }
